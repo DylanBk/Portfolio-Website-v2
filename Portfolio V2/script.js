59bc1291effc2d9aabb407fd73929df2541d9a47
Dylan <144438972+DylanBk@users.noreply.github.com>
2024-01-23 21:01:03 +0000

    Delete Portfolio V2 directory

--- Portfolio V2/script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 320 || document.documentElement.scrollTop > 320) {
-    document.getElementById("navbar").style.height = "6rem";
-  } else {
-    document.getElementById("navbar").style.height = "4rem";
-  }
-}
